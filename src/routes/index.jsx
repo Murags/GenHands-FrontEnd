3022c8a276d311031a91a3918de513018bb5b6d8
Dennis Murage <dmurage140@gmail.com>
2025-05-14 19:57:02 +0300

    API con setup

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
+import ApiStatusPage from '../pages/ApiStatusPage/ApiStatusPage';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/api-status" element={<ApiStatusPage />} />
     </Routes>
   );
 }
