65aeb2f8aa152cd84f0def83250817ddba201658
Dennis Murage <dmurage140@gmail.com>
2025-06-15 16:40:21 +0300

    Implementing fetch for charities with custom hooks and an autocomplete component for search

---
+++ src/services/charityService.js
@@ -0,0 +1,14 @@
+import httpService from './httpService';
+
+const charityApiEndpoint = 'auth/charities';
+
+export const getCharities = async () => {
+  try {
+    const response = await httpService.get(charityApiEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching charities:', error);
+
+    throw error;
+  }
+};
