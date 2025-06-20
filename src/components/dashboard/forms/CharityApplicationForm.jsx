1992d210b34dc82e617e3592529836970ef5bee6
jannyfromtechsupport <janny.jonyo@strathmore.edu>
2025-06-20 16:21:42 +0300

    Added toast messages for email notifications. Also modified aesthetics of donor dashboard slightly

--- src/components/dashboard/forms/CharityApplicationForm.jsx
+++ src/components/dashboard/forms/CharityApplicationForm.jsx
@@ -40,7 +40,7 @@ const CharityApplicationForm = ({ charity, onCloseDrawer }) => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['charities'] });
       const statusText = currentStatus === 'Approved' ? 'approved' : 'rejected';
-      toast.success(`Charity application ${statusText} successfully!`);
+      toast.success(`Charity application ${statusText} successfully and email notification sent!`);
       onCloseDrawer();
     },
     onError: (err) => {
@@ -106,7 +106,7 @@ const CharityApplicationForm = ({ charity, onCloseDrawer }) => {
                 // If doc is a string, treat it as a file path
                 const isString = typeof doc === 'string';
                 const url = isString
-                  ? `http://localhost:3000/${doc.replace(/^\/+/, '')}` // Adjust base URL if needed
+                  ? `http://localhost:3000/${doc.replace(/^\/+/, '')}`
                   : doc.url;
                 const name = isString
                   ? doc.split('/').pop()
